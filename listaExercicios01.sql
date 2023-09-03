ab5bddd5058317fb5f021374ffd9f7ad45b65d0f
nicole-santana <nicole.teles.santana@gmail.com>
2023-09-03 14:01:35 -0300

    ex2

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,8 +1,5 @@
 -- Exercícios da lista:
 
--- Ex1:
+-- Ex2:
 
-select livros.titulo from aula_db_exemplos.livros;
-
-
-
+select autores.nome from aula_db_exemplos.autores where autores.nascimento < '1901-01-01';
