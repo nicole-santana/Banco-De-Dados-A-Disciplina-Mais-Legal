2027670705e1793179b48c6f0625dc17de080f51
nicole-santana <nicole.teles.santana@gmail.com>
2023-09-03 15:41:32 -0300

    ex4

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,5 +1,6 @@
 -- Exercícios da lista:
 
--- Ex2:
 
-select autores.nome from aula_db_exemplos.autores where autores.nascimento < '1901-01-01';
+-- Ex4
+
+select alunos.nome from alunos inner join matriculas on alunos.id = matriculas.aluno_id where curso = 'Engenharia de Software';
