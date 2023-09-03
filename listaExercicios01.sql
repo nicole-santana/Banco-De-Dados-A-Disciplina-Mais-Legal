5fc98b5a5f9e33d15cf230210c9be78b0c123d52
nicole-santana <nicole.teles.santana@gmail.com>
2023-09-03 20:12:45 -0300

    ex11

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,5 +1,5 @@
 -- Exercícios da lista
 
--- ex 10
+-- ex 11
 
-select nome, count(titulo) as n°_de_livros from autores inner join livros where autores.id = livros.autor_id group by nome having n°_de_livros >2;
+select titulo, nome from livros inner join autores on livros.autor_id=autores.id;
