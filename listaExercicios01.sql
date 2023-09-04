6945ebd8169cdfba08c5ab0c6a6a55ba64badde1
nicole-santana <nicole.teles.santana@gmail.com>
2023-09-04 10:07:03 -0300

    ex19

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,17 +1,5 @@
 -- Exercícios da lista
 
--- ex 18
+-- ex 19
 
-ALTER TABLE vendas ADD id_a int(4);
-UPDATE vendas SET id_a = 1 WHERE id in (1, 4, 10);
-UPDATE vendas SET id_a = 2 WHERE id in (2,5,11);
-UPDATE vendas SET id_a = 3 WHERE id in (3, 6, 13);
-UPDATE vendas SET id_a = 4 WHERE id in (7, 14);
-UPDATE vendas SET id_a = 5 WHERE id = 8;
-UPDATE vendas SET id_a = 6 WHERE id = 9;
-
-with total_de_vendas as (
-	select count(produto)*20 as produtoT from vendas  inner join autores on vendas.id_a = autores.id group by produto
-)
-
-select produtoT, nome from total_de_vendas inner join autores;
+select nome, count(curso) as n°_de_matriculas from alunos inner join matriculas on alunos.id = matriculas.aluno_id group by nome;
