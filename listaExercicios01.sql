f854da134f911c1fb3861c2ef63b2c9b35eea274
nicole-santana <nicole.teles.santana@gmail.com>
2023-09-04 10:08:14 -0300

    ex20

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,5 +1,8 @@
 -- Exercícios da lista
 
--- ex 19
+-- ex 20
 
-select nome, count(curso) as n°_de_matriculas from alunos inner join matriculas on alunos.id = matriculas.aluno_id group by nome;
+with n°_de_produtos as (
+    select produto, count(*) as n° from vendas group by produto
+)
+select produto, n° from n°_de_produtos where n° = (select max(n°) from n°_de_produtos);
